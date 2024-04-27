32e3f542bb36ce1921097de541c258954cbbefd2
rebpacquiao <reb.pacquiao@gmail.com>
2024-04-27 09:16:43 +0800

    Add initial project files and configurations

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+    "node_modules/flowbite-react/lib/esm/**/*.js",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        dark: "#000",
+        clear: "#F0EFEF",
+        darkOrange: "#F28E05",
+        mediumRisk: "#F2B705",
+        confirm: "#808080",
+      },
+    },
+  },
+};
